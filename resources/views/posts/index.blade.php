7cdfd87111acadc2ec0f63e2ac393c461c9c324b
Liam <liam.pesser@gmail.com>
2021-01-17 19:50:37 +0100

    Mise en place de la zone dynamique

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,32 @@
+<!-- ZONE DYNAMIQUE  -->
+
+@extends('templates.index')
+
+@section('title')
+  Liste des posts
+@endsection
+
+@section('content')
+  @foreach ($posts as $post)
+    <div class="col-md-6 d-flex ftco-animate">
+      <div class="blog-entry justify-content-end">
+        <a href="article.html" class="block-20" style="background-image: url('images/image_1.jpg');">
+        </a>
+        <div class="text p-4 float-right d-block">
+          <div class="topper d-flex align-items-center">
+            <div class="one py-2 pl-3 pr-1 align-self-stretch">
+              <span class="day">18</span>
+            </div>
+            <div class="two pl-0 pr-3 py-2 align-self-stretch">
+              <span class="yr">2019</span>
+              <span class="mos">October</span>
+            </div>
+          </div>
+          <h3 class="heading mb-3"><a href="#">Article title</a></h3>
+          <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+          <p><a href="article.html" class="btn-custom"><span class="ion-ios-arrow-round-forward mr-3"></span>Read more</a></p>
+        </div>
+      </div>
+    </div>
+  @endforeach
+@endsection
