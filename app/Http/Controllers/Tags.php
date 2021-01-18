892b2e53df7da5687bf183bddc201ed383111ea7
Liam <liam.pesser@gmail.com>
2021-01-18 12:42:55 +0100

    Route posts.show - Tags & Author

--- app/Http/Controllers/Tags.php
+++ app/Http/Controllers/Tags.php
@@ -7,4 +7,7 @@ use Illuminate\Http\Request;
 class Tags extends Controller
 {
     //
+    $tags = Tag::orderBy('name', 'ASC')
+    ->get();
+    return view('tags.index', compact(tags))
 }
---
+++ resources/views/tags/_post_tags.blade.php
@@ -0,0 +1,11 @@
+{{--
+  Variables disponibles:
+    - $tags ARRAY(Tag)
+ --}}
+<div class="tag-widget post-tag-container mb-5 mt-5">
+  <div class="tagcloud">
+    @foreach ($tags as $tag)
+      <a href="#" class="tag-cloud-link">{{ $tag->name }}</a>
+    @endforeach
+  </div>
+</div>
