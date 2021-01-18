bbbe6ae60a341285c8d3bf6d5489b3e5fb849094
Liam <liam.pesser@gmail.com>
2021-01-18 15:46:14 +0100

    Contacts - Form

--- resources/views/templates/partials/_nav.blade.php
+++ resources/views/templates/partials/_nav.blade.php
@@ -10,7 +10,7 @@
     <div class="collapse navbar-collapse" id="ftco-nav">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active"><a href="{{ route('blog') }}" class="nav-link">Home</a></li>
-        <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+        <li class="nav-item"><a href="{{ route('contacts.form') }}" class="nav-link">Contact</a></li>
       </ul>
     </div>
   </div>
